49af40e0bfe41a86dea3648b296fe1a7d4ffa403
Oussama Rafouk <156216265+ocryptocode@users.noreply.github.com>
2024-07-09 10:55:06 +0100

    Update .eslintrc.js
    Add documentation on it

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,44 @@
+/**
+ * ESLint configuration file for a project using TypeScript, Prettier, and Jest.
+ */
+
 module.exports = {
+  // Specifies the environments your script is designed to run in.
   env: {
-    browser: true,
-    es6: true,
-    node: true,
+    browser: true, // Enables browser global variables like 'window' and 'document'.
+    es6: true, // Enables ES6 global variables and syntax.
+    node: true, // Enables Node.js global variables like 'global' and 'process'.
   },
+  // Extends the base configurations from various ESLint plugins.
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:jest/recommended',
-    'plugin:prettier/recommended',
+    'eslint:recommended', // Uses the recommended rules from ESLint.
+    'plugin:@typescript-eslint/eslint-recommended', // Uses the recommended rules from @typescript-eslint.
+    'plugin:jest/recommended', // Uses the recommended rules for Jest.
+    'plugin:prettier/recommended', // Uses the recommended rules for Prettier to ensure code formatting.
   ],
+  // Defines global variables that are read-only.
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: 'readonly', // Adds Atomics as a read-only global variable.
+    SharedArrayBuffer: 'readonly', // Adds SharedArrayBuffer as a read-only global variable.
   },
-  parser: '@typescript-eslint/parser',
+  // Specifies the parser and its options.
+  parser: '@typescript-eslint/parser', // Specifies @typescript-eslint/parser as the parser to support TypeScript.
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
+    ecmaVersion: 2018, // Specifies the version of ECMAScript syntax to use (ES9).
+    sourceType: 'module', // Allows the use of ECMAScript modules.
   },
-  plugins: ['@typescript-eslint', 'jest', 'prettier'],
+  // Lists the plugins to be used.
+  plugins: [
+    '@typescript-eslint', // Adds TypeScript support to ESLint.
+    'jest', // Adds Jest support.
+    'prettier', // Adds Prettier support.
+  ],
+  // Customizes the behavior of individual ESLint rules.
   rules: {
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'double'],
-    semi: ['error', 'always'],
-    'no-unused-vars': 'off',
-    'no-prototype-builtins': 'off',
+    'linebreak-style': ['error', 'unix'], // Enforces Unix line endings (LF).
+    quotes: ['error', 'double'], // Enforces the use of double quotes for strings.
+    semi: ['error', 'always'], // Enforces the use of semicolons at the end of statements.
+    'no-unused-vars': 'off', // Disables the rule for unused variables (handled by TypeScript).
+    'no-prototype-builtins': 'off', // Disables the rule against calling Object.prototype methods directly on objects.
   },
 }
